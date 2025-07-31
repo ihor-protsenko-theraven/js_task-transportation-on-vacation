e3b7565ddfe011f48903ea41f3aacb19adf8c53f
ihor.protsenko <ihor.protsenko@gaimin.io>
2025-07-31 19:55:08 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseRentalPerDay = 40;
+  const discountThreeDays = 20;
+  const cashbackByWeek = 50;
+  const cost = baseRentalPerDay * days;
+
+  if (days >= 7) {
+    return cost - cashbackByWeek;
+  }
+
+  if (days >= 3) {
+    return cost - discountThreeDays;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
